e6215eee593b200630344a878a12f10982eb78d5
mobinEzzati <mobinezzat662@gmail.com>
2019-05-26 00:50:15 +0430

    Initial Commit

---
+++ MobinMusicPlayerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobinMusicPlayerApp
+//
+//  Created by mobin on 5/26/19.
+//  Copyright © 2019 SpyderInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
